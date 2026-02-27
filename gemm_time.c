2bfe1c434b2e20ec59bd2f4e036949e4679e0805
gddeazevedo <gabrielcapuerj@gmail.com>
2026-02-27 06:48:18 -0300

    implementando e testando GEMM sem paralelismo e com SIMD. Renomeação de tipo de funçõ de callback

--- gemm_time.c
+++ gemm_time.c
@@ -5,7 +5,7 @@
 
 #define IDX(i, j) i*n + j
 
-typedef void (*gemm)(double *C, const double *A, const double *B, const double alpha, const double beta, const uint32_t n);
+typedef void (*gemm_func_t)(double *C, const double *A, const double *B, const double alpha, const double beta, const uint32_t n);
 
 static inline double wtime() {
     struct timespec t;
@@ -76,6 +76,25 @@ void pro_gemm_parallel(double *C, const double *A, const double *B, const double
     free(Bt);
 }
 
+void pro_gemm_simd(double *C, const double *A, const double *B, const double alpha, const double beta, const uint32_t n) {
+    double *Bt = tranpose(B, n);
+
+    for (uint32_t i = 0; i < n; i++) {
+        for (uint32_t j = 0; j < n; j++) {
+            double sum = 0.0;
+
+            #pragma omp simd reduction(+:sum)
+            for (uint32_t k = 0; k < n; k++) {
+                sum += A[IDX(i, k)] * Bt[IDX(j, k)];
+            }
+
+            C[IDX(i, j)] = alpha * sum + beta * C[IDX(i, j)];
+        }
+    }
+
+    free(Bt);
+}
+
 void pro_gemm_parallel_simd(double *C, const double *A, const double *B, const double alpha, const double beta, const uint32_t n) {
     double *Bt = tranpose(B, n);
 
@@ -96,7 +115,7 @@ void pro_gemm_parallel_simd(double *C, const double *A, const double *B, const d
     free(Bt);
 }
 
-void run_gemm_benchmark(uint32_t n, gemm func, char *label) {
+void run_gemm_benchmark(uint32_t n, gemm_func_t gemm, char *label) {
     double *A = malloc(n * n * sizeof(double));
     double *B = malloc(n * n * sizeof(double));
     double *C = malloc(n * n * sizeof(double));
@@ -110,7 +129,7 @@ void run_gemm_benchmark(uint32_t n, gemm func, char *label) {
     }
 
     double start = wtime();
-    func(C, A, B, 1.0, 1.0, n);
+    gemm(C, A, B, 1.0, 1.0, n);
     double end = wtime();
     printf("%s: %lf seconds\n", label, end - start);
 
@@ -130,6 +149,7 @@ int main(int argc, char **argv) {
     run_gemm_benchmark(n, naive_gemm, "Naive GEMM");
     run_gemm_benchmark(n, pro_gemm, "Pro GEMM");
     run_gemm_benchmark(n, pro_gemm_parallel, "Pro GEMM Parallel");
+    run_gemm_benchmark(n, pro_gemm_simd, "Pro GEMM SIMD");
     run_gemm_benchmark(n, pro_gemm_parallel_simd, "Pro GEMM Parallel SIMD");
 
     return 0;
